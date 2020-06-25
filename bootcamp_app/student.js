4241edd40877d8a83a2ae0e6ccd21e0f90cab0b7
Rishav Baral <rishavbaral6@gmail.com>
2020-06-25 07:42:10 +0000

    parameterize my queries to protect from sql injections

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -7,23 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// let userInputDate = process.argv[2];
-let userInputDate = process.argv[2]
-pool.query(`
-SELECT DISTINCT cohorts.name AS cohort, teachers.name AS teacher
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${userInputDate}%'
-ORDER BY teachers.name
-`)
-.then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
 
 
+
+// my code
 
 // let userInputDate = process.argv[2];
 // let userInputNum = process.argv[3];
@@ -39,15 +26,33 @@ ORDER BY teachers.name
 // })
 // .catch(err => console.error('query error', err.stack));
 
-// pool.query(`
-// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//   })
-// });
+// class code
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+
+
+
+
+
+
+
 
