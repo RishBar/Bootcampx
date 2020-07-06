01307290bf4031c9dd04d295c4ac1b9776694298
Rishav Baral <rishavbaral6@gmail.com>
2020-07-05 19:19:50 -0700

    testing if git works

--- 4_queries/total_cohort_assistance_duration.sql
+++ 4_queries/total_cohort_assistance_duration.sql
@@ -3,4 +3,4 @@ FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
-ORDER BY total_duration;
+ORDER BY total_duration;ø
